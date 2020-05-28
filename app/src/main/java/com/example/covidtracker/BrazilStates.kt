6d87189ca710a99e29450cd283400426d4d54f67
Johann Schallenberger <schajohann@yahoo.com.br>
2020-05-27 23:23:08 -0300

    Second Commit - Fetch Json

--- app/src/main/java/com/example/covidtracker/BrazilStates.kt
+++ app/src/main/java/com/example/covidtracker/BrazilStates.kt
@@ -4,7 +4,12 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.GsonBuilder
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.synthetic.main.activity_brazil_states.*
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class BrazilStates : AppCompatActivity() {
 
@@ -16,5 +21,34 @@ class BrazilStates : AppCompatActivity() {
 
         statesRecView.layoutManager = LinearLayoutManager(this)
         statesRecView.adapter = StatesAdapter()
+
+        fetchJson()
     }
+
+    fun fetchJson(){
+        val url = "https://covid19-brazil-api.now.sh/api/report/v1"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object: Callback{
+            override fun onResponse(call: Call, response: Response) {
+               val body = response.body!!.string()
+                println(body)
+                //val json = JSONObject(response.body!!.string())
+                //val estados = json.getJSONArray("data")
+                val gson = GsonBuilder().create()
+
+                val dataCovid = gson.fromJson(body, DataCovid::class.java)
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Errorrrrrr")
+            }
+        })
+    }
+
+    class DataCovid(val data: List<States>)
+
+    class States(val uid: Int, val uf: String)
+
 }
